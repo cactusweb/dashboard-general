db5347e31ea38ea554ba7fdb1299d55ef7ccdd77
gaspromov <gaspromov@mail.ru>
2024-01-26 23:55:00 +0300

    [new] smoothscroll

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import * as smoothscroll from 'smoothscroll-polyfill';
 
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+smoothscroll.polyfill();
