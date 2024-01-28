0ebc7129bcc27f214653eb28568635532136ea16
gaspromov <gaspromov@mail.ru>
2024-01-28 11:15:36 +0300

    [minor] Text changed

--- src/app/common/interceptors/error.enums.ts
+++ src/app/common/interceptors/error.enums.ts
@@ -1,5 +1,5 @@
 export enum ErrorEnum {
-  ConnectionTimeout = 'Connection error. Make sure the quality of the connection.',
+  ConnectionTimeout = 'Connection timeout. Check you internet connection.',
   ServerUnavailable = 'The server is temporarily unavailable.',
   NotFound = 'Bad request: Endpoint not found.',
   NotAuth = 'Error 401: You are not authorized.',
