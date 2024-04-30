72dff3eeefeba1abd50b0361c4d8c6e7b923f499
gaspromov <gaspromov@mail.ru>
2024-04-30 23:52:57 +0300

    [fix] Window.open => window.location.href =

--- src/app/nft-verification/common/services/mobile-solana/utils/mobile-solana.utils.ts
+++ src/app/nft-verification/common/services/mobile-solana/utils/mobile-solana.utils.ts
@@ -73,7 +73,7 @@ function useMobileSolanaMethod(
   });
 
   const url = `https://phantom.app/ul/v1/${method}?${params.toString()}`;
-  window.open(url, '_self');
+  window.location.href = url;
 }
 
 export { useMobileSolanaMethod, decryptMobileSolanaResponse };
