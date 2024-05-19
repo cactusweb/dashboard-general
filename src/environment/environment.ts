9456c66111fcb9d8b6bc4c6b4ada3875124ec8e7
gaspromov <gaspromov@mail.ru>
2024-05-19 12:45:21 +0300

    minor

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.cactusweb.io/api/v2/user',
-  // apiUrl: 'https://test-api.cactusweb.io/api/v2/user',
+  apiUrl: 'https://test-api.cactusweb.io/api/v2/user',
   siteUrl: 'http://localhost:4200',
 };
